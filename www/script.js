560affbbbfd1807910f4c35c5a8a215a61f2d1d2
Eric Cimino <design.eric.cimino@gmail.com>
2023-07-05 21:34:00 -0500

    cleanup, add favicon, cardinal sin

--- www/script.js
+++ www/script.js
@@ -3,3 +3,12 @@ window.onload = function () {
     var targetPosition = document.body.scrollHeight; // Adjust the value to position the header
     window.scrollTo(0, targetPosition);
 };
+
+// var minScrollPosition = 500; // Define the minimum scroll position from the top
+
+// window.addEventListener('scroll', function (event) {
+//     if (window.scrollY < minScrollPosition) {
+//         event.preventDefault();
+//         window.scrollTo(0, minScrollPosition);
+//     }
+// });
